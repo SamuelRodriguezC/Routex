53bb09e991aa2383fee84131295e3c0ec6593730
SamuelRodríguezC <samuelrc250@gmail.com>
2026-04-14 17:44:11 -0500

    Conexión a base de datos Mysql

--- backend/routexapi/settings.py
+++ backend/routexapi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'routexapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
