d49446f10fa52e89a4d05482890537e035ae1c98
SamuelRodríguezC <samuelrc250@gmail.com>
2026-04-16 02:38:31 -0500

    ajuste en appconfig.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { providePrimeNG } from 'primeng/config';
 import { provideHttpClient, withFetch  } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import Aura from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 // import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
