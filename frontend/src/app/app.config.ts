8bbf321af863aaad7a5f6e686b1f8fe046d0d0fa
SamuelRodríguezC <samuelrc250@gmail.com>
2026-04-16 01:58:11 -0500

    Instalación de primeng

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
 import { provideHttpClient, withFetch  } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Aura from 'primeng/config';
+// import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+    // provideAnimations(),
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withFetch()),
     provideRouter(routes), provideClientHydration(withEventReplay())
